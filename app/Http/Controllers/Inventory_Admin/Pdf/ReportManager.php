0dbd97dfffcb5b9027a046001522055c5e330605
unknown <aducajan@gmail.com>
2025-03-20 09:12:59 +0800

    Fixed Report

--- app/Http/Controllers/Inventory_Admin/Pdf/ReportManager.php
+++ app/Http/Controllers/Inventory_Admin/Pdf/ReportManager.php
@@ -10,7 +10,7 @@ use App\Models\ItemModel;
 use App\Models\ReceiveModel;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Validator;
-
+use Carbon\Carbon;
 class ReportManager extends Controller
 {
     public function generateReport(Request $request){
@@ -26,20 +26,34 @@ class ReportManager extends Controller
             $response = $request->get('period');
             switch ($response) {
                 case 'Monthly':
-                    $month = $request->get('month');
+                    
+                    $month = $request->get('month');  
+
                     $items = ItemModel::with([
                         'receives' => function ($query) use ($month) {
                             $query->where('received_month', $month);
                         },
                         'inventory.unit'
                     ])->get();
-
                     
+                    $month = $request->get('month');
+                    $year = Carbon::now('Asia/Manila')->year;  
+                    $date = Carbon::createFromFormat('Y-F', "$year-$month");   
+                    $subDate = Carbon::createFromFormat('Y-F', "$year-$month");   
+                    $currentDate = $date->endOfMonth();
+                    $subMonth = $subDate->subMonths(1);
+                    $modifiedDate = $subMonth->endOfMonth(); 
+                    $formattedDate = $modifiedDate->format('m/d/Y');
+                    $formattedCurrentDate =$currentDate->format('m/d/Y');
+
                     $inventories = InventoryModel::all();
+                    $capsMonth = strtoupper($month);
                     $data = [
-                        'title' => 'Monthly Inventory Report',
+                        'title' => 'INVENTORY REPORT OF SUPPLIES FOR THE MONTH OF ' . $capsMonth,
                         'items' => $items,
-                        'inventories' => $inventories
+                        'inventories' => $inventories,
+                        'formattedDate' => $formattedDate,
+                        'formattedCurrentDate' => $formattedCurrentDate
                     ];
     
                     $pdf = PDF::loadView('admin.pdf.monthly-report', $data)
@@ -48,6 +62,10 @@ class ReportManager extends Controller
                             'isHtml5ParserEnabled' => true,
                             'isRemoteEnabled' => true,
                             'defaultFont' => 'sans-serif',
+                            'margin-top' => 10,       // Top margin (in points)
+                            'margin-right' => 20,     // Right margin (in points)
+                            'margin-bottom' => 10,    // Bottom margin (in points)
+                            'margin-left' => 20,      // Left margin (in points)
                         ]);
                     return $pdf->download(time() . '.pdf');
             }
--- resources/views/admin/pdf/monthly-report.blade.php
+++ resources/views/admin/pdf/monthly-report.blade.php
@@ -7,16 +7,17 @@
     <title>Monthly Report</title>
 </head>
 <body>
-    <table border="2">
+    <h1>{{ $title }}</h1>
+    <table border="2" width="100%" class="table table-responsive">
        <thead>
             <tr>
                 <th>No.</th>
                 <th></th>
                 <th>Units</th>
-                <th>Balance as of </th>
+                <th>Balance as of {{ $formattedDate }}</th>
                 <th>Total Supply Received</th>
                 <th>Total Supply Withdrawn</th>
-                <th>Balance as of</th>
+                <th>Balance as of {{ $formattedCurrentDate }}</th>
             </tr>
        </thead>
        <tbody>
@@ -36,6 +37,8 @@
                         <td>{{ $item->inventory->unit->name }}</td>
                         <td>{{ $item->inventory->quantity}}</td>
                         <td>{{ $item->receives->sum('received_quantity') }}</td>
+                        <td>{{ $item->inventory->quantity }}</td>
+                        <td>{{ $total = $item->receives->sum('received_quantity') - $item->inventory->quantity}}</td>
                     </tr>
                 @endforeach
             @endforeach
